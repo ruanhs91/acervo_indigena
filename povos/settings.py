481408ffa9c5fb95a3beb5ce2843d18b9cb6e5f8
ruanhs91 <ruanhenry139@gmail.com>
2025-11-29 22:27:42 -0300

    dockerização em postgres (não rodou no meu notebook)

--- povos/settings.py
+++ povos/settings.py
@@ -86,6 +86,8 @@ DATABASES = {
     }
 }
 
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST', 'localhost')
+POSTGRES_PORT = os.environ.get('POSTGRES_PORT', '5432')
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
